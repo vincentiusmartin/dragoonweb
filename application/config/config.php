859f1293383e0cf0c797b1e413c973dd0dea8c7d
Nikodemus Adriel <a.limanthie@yahoo.com>
2014-03-15 21:15:07 +0700

    add secure encryption key

--- application/config/config.php
+++ application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = 'REALLY_LONG_NUMBER';
+$config['encryption_key'] = '3rISu0UAxv9cKtby5F2m64GPJsvwsvvMLD0yXFtWEBSdQg1CXXuxRYCjJzoXtdm';
 
 /*
 |--------------------------------------------------------------------------
